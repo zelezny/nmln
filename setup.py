a111feda8b6a2191e2a37eef745cfb539b3fa117
Filip Zelezny @potato <zelezny@cvut.cz>
2024-02-06 10:52:53 +0100

    fix install problem

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='nmln',
+      packages=find_packages(),
       version='0.0.1',
       install_requires=['tensorflow>=2.0',
                         'sortedcontainers',
